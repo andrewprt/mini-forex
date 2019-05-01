4f360f1f4c9f9a94c4f0c45ca566b652fb1e9d53
andrewprt <andrew.prawira.tunggono@gmail.com>
2019-05-01 15:43:08 +0700

    Updates

---
+++ precache-manifest.4430f89caafeb5c825db560c7d795a5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f448f42df58a7fa5a84f7d10667fe1",
+    "url": "/mini-forex/index.html"
+  },
+  {
+    "revision": "fc45790045738445ceba",
+    "url": "/mini-forex/static/css/main.301c57cf.chunk.css"
+  },
+  {
+    "revision": "4b2d36830b28ad41cf57",
+    "url": "/mini-forex/static/js/2.2552aa6d.chunk.js"
+  },
+  {
+    "revision": "fc45790045738445ceba",
+    "url": "/mini-forex/static/js/main.b4ca8782.chunk.js"
+  },
+  {
+    "revision": "395d1b90613100308cf8",
+    "url": "/mini-forex/static/js/runtime~main.f1a29caa.js"
+  }
+]);
